7b5666272b583cf4da15f507776ff673afc8ba47
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-04 18:46:42 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,15 @@
-a = 1
-b = 2
+def add(a, b):
+  """
+  Adds two numbers together.
 
-from add_0 import add
+  Args:
+    a: The first number to add.
+    b: The second number to add.
 
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+  Returns:
+    The sum of the two numbers.
+  """
+
+  return a + b
+
+
--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,1 +1,7 @@
-__import__("0-add")
+import add_0
+
+a = 1
+b = 2
+
+print(f"{a} + {b} = {add_0.add(a, b)}")
+
