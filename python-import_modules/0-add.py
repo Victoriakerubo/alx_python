bc196e5d388f2ff048c984120ede2f7920d425e4
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-06 15:38:49 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 a = 1
 b = 2
+
 if __name__ == "__main__":
-from add_0 import sum
-result = sum(a, b)
-print("1 + 2 =", result)
-
-
+    from add_0 import add
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
 
