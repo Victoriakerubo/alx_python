c4e009cfd4b171aef3a6d3688551fa59a4744917
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-06 13:04:20 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,4 @@
-def add(a, b):
-  """
-  Adds two numbers together.
 
-  Args:
-    a: The first number to add.
-    b: The second number to add.
 
-  Returns:
-    The sum of the two numbers.
-  """
-
-  return a + b
 
 
