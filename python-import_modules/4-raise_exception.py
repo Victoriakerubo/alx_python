27ff7c9b9959cf9805afb40ec4d641c2c277f0db
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-06 16:22:33 +0300

    commit

---
+++ python-import_modules/4-raise_exception.py
@@ -0,0 +1,8 @@
+def raise_exception():
+    raise TypeError("An exception has been raised")
+
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
