f4201e27da3b8a1d7ab4167bbdee74d11dc4d84f
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-06 18:20:42 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,11 @@
 class Square:
     def __init__(self, size):
-        self.__size = size
+        self.__size = int(size)
+
+    def area(self):
+        return self.__size * self.__size
+
+
+if __name__ == "__main__":
+    square = Square(3)
+    print(square.area())
