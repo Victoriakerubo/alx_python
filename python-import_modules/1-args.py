1fe90d538cfbafa3cbc78b0a413e2235d6877dad
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-06 16:30:50 +0300

    commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,15 +1,15 @@
 import sys
 
-def print_arguments(arguments):
-    """Prints the arguments passed to the script."""
-    number_of_arguments = len(arguments) - 1  # Exclude the script name
-    print("{} argument{}:".format(number_of_arguments, "s" if number_of_arguments != 1 else ""), end="")
-    if number_of_arguments == 0:
-        print(".")
-    else:
-        for index, argument in enumerate(arguments[1:], start=1):  # Exclude the script name
-            print("\n{}: {}".format(index, argument))
-
 if __name__ == "__main__":
-    arguments = sys.argv
-    print_arguments(arguments)
+    args = sys.argv[1:]  # Exclude the script name from the arguments list
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
