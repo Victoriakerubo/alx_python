76fa1bddb6803669621a2d72c2552b0bafd81a79
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-07 19:25:52 +0300

    commit

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,13 @@
+def is_kind_of_class(obj, a_class):
+
+
+
+  """Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False."""
+
+  if isinstance(obj, a_class):
+    return True
+  else:
+    for c in a_class.__bases__:
+      if is_kind_of_class(obj, c):
+        return True
+  return False
