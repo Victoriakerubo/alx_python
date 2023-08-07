58c0dc672742afc036440cd36ac70adefbeaa72c
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-07 19:45:09 +0300

    commit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,10 +1,18 @@
-is_kind_of_class = __import__('1-is_kind_of_class').is_kind_of_class
+def is_kind_of_class(obj, class_type):
+    """Returns True if obj is an instance of class_type or a subclass of class_type."""
+    if isinstance(obj, class_type):
+        return True
+    elif isinstance(obj, type):
+        return issubclass(obj, class_type)
+    else:
+        return False
 
 a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+
+if is_kind_of_class(type(a), int):
+    print(f"The integer {a} comes from the int class")
+if is_kind_of_class(type(a), float):
+    print(f"The integer {a} comes from the float class")
+if is_kind_of_class(type(a), object):
+    print(f"The integer {a} comes from the object class")
 
