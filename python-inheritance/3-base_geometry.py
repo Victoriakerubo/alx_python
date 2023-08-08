8282cc7bf294a39722e47a22e894a5677f310ef6
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-08 15:43:06 +0300

    commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,2 +1,9 @@
+# File: 3-base_geometry.py
+
 class BaseGeometry:
+    """
+    This is the BaseGeometry class.
+
+    It serves as the base class for geometry-related operations.
+    """
     pass
