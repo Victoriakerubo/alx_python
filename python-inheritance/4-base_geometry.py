742964650f555fdb273b1082952df615e6669a0d
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-08 15:49:56 +0300

    commit

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,16 @@
+# File: 4-base_geometry.py
+
+class BaseGeometry:
+    """
+    This is the BaseGeometry class.
+
+    It serves as the base class for geometry-related operations.
+    """
+
+    def area(self):
+        """
+        Public instance method to calculate the area.
+
+        This method raises an exception indicating that it is not implemented.
+        """
+        raise Exception("area() is not implemented")
