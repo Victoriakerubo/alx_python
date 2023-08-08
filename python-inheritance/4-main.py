2adc37e8d18cba2b268aab8f71788cb27419527b
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-08 15:51:25 +0300

    commit

---
+++ python-inheritance/4-main.py
@@ -0,0 +1,10 @@
+# File: 4-main.py
+
+BaseGeometry = __import__('4-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
