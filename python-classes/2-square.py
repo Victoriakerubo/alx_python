753d5207ca737453a61a60f9e60fece20d707f0e
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-11 15:14:32 +0300

    commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,44 +1,31 @@
+#!/usr/bin/python3
+
 class Square:
     """
-    This class defines a square by its size.
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
     """
 
     def __init__(self, size=0):
         """
-        Initializes a new Square instance.
+        Initializes a new instance of the Square class.
 
         Args:
-            size (int, optional): The size of the square. Defaults to 0.
+            size (int): The size of the square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
         """
-        Calculate the area of the square.
+        Calculates the area of the square.
 
         Returns:
             int: The area of the square.
         """
         return self.__size ** 2
-
-# Test the Square class
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print("Area: {}".format(my_square_1.area()))
-
-    try:
-        print(my_square_1.size)
-    except AttributeError as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except AttributeError as e:
-        print(e)
-
-    my_square_2 = Square(5)
-    print("Area: {}".format(my_square_2.area()))
