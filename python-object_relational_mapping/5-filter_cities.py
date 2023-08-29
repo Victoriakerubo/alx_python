ddfdb53a019b0b3dd53539d34239554bd6482d22
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-29 18:45:01 +0300

    commit

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa"""
+"""Lists all cities of a given state from the database hbtn_0e_4_usa"""
 
 import sys
 import MySQLdb
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     # Connect to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
@@ -17,21 +18,21 @@ if __name__ == "__main__":
     # Create a cursor
     cursor = db.cursor()
 
-    # Execute the query
+    # Prepare the query with placeholders and safely execute
     query = """
-    SELECT cities.id, cities.name, states.name
+    SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ')
     FROM cities
     JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
+    WHERE states.name = %s
     """
-    cursor.execute(query)
+    cursor.execute(query, (state_name,))
 
-    # Fetch all the rows
-    rows = cursor.fetchall()
+    # Fetch the result
+    result = cursor.fetchone()
 
     # Display the results
-    for row in rows:
-        print(row)
+    if result and result[0]:
+        print(result[0])
 
     # Close the cursor and connection
     cursor.close()
