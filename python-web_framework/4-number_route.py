b9840cdf6acb1cc1d9c3eda7c7bcbfc6fab518ae
Victoriakerubo <victoriakerubog@gmail.com>
2023-08-30 18:29:58 +0300

    commit

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Starts a Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!' when accessing root."""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Displays 'HBNB' when accessing /hbnb."""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Displays 'C' followed by the value of the text variable."""
+    return "C " + text.replace('_', ' ')
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """Displays 'Python' followed by the value of the text variable (or default)."""
+    return "Python " + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Displays 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
