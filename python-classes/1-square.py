732c5b07711a828b667c22394c495587dbed3ee9
Victoriakerubo <victoriakerubog@gmail.com>
2023-10-09 17:34:44 +0300

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,22 +1,43 @@
-#!/usr/bin/python3
-
 class Square:
-    """
-    This class represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
+    """This is a Square class."""
 
     def __init__(self, size=0):
-        """
-        Initializes a new instance of the Square class.
-
-        Args:
-            size (int): The size of the square.
-        """
+        """Initialize a Square instance with a given size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print(type(my_square_1))
+    print(my_square_1.__dict__)
+
+    my_square_2 = Square()
+    print(type(my_square_2))
+    print(my_square_2.__dict__)
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1._Square__size)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_3 = Square("3")
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_4 = Square(-89)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
+    except Exception as e:
+        print(e)
