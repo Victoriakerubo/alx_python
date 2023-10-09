cdc955f5b1944eaaaafa16fcd68b893ca665b912
Victoriakerubo <victoriakerubog@gmail.com>
2023-10-09 17:27:47 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
+    
     """This is a Square class."""
 
     def __init__(self, size):
+
         """Initialize a Square instance with a given size."""
+
         self.__size = size
 
