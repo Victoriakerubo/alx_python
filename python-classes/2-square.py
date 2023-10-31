53739b87172b0f0b1b16262d7b2d2248fa382411
Victoriakerubo <victoriakerubog@gmail.com>
2023-10-31 15:32:16 +0300

    commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,19 +1,44 @@
-#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Square Module
+
+This module defines a `Square` class that represents a square with a size attribute.
+The size attribute is validated to ensure it is an integer and greater than or equal to 0. 
+The class also provides a method for calculating the area of the square.
+
+Example:
+    You can create an instance of the `Square` class by specifying its size and calculate
+    the area of the square.
+
+Attributes:
+    None
+
+Todo:
+    * Implement methods for calculating the perimeter and other properties of the square.
+
+"""
 
 class Square:
-    """
-    This class represents a square.
+    """A class to represent a square.
+
+    This class allows you to create a square by specifying its size. The size
+    is a private attribute, and this class provides methods for working with
+    the square.
 
     Attributes:
         __size (int): The size of the square.
+
     """
 
     def __init__(self, size=0):
-        """
-        Initializes a new instance of the Square class.
+        """Initialize a Square with the given size.
 
         Args:
-            size (int): The size of the square.
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -22,10 +47,13 @@ class Square:
         self.__size = size
 
     def area(self):
-        """
-        Calculates the area of the square.
+        """Calculate the area of the square.
 
         Returns:
-            int: The area of the square.
+            int: The area of the square (size squared).
+
         """
         return self.__size ** 2
+
+# Additional documentation for methods, attributes, and functions can be added as needed.
+
