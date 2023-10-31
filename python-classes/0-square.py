e4c61bdc71913bd3b836238ff21b806013d71d0f
Victoriakerubo <victoriakerubog@gmail.com>
2023-10-31 15:16:53 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,44 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Square Module
+
+This module defines a `Square` class that represents a square and its size.
+
+Example:
+    You can create an instance of the `Square` class by specifying its size and
+    then use it to perform various operations on the square.
+
+Attributes:
+    None
+
+Todo:
+    * Add methods for calculating the area and perimeter of the square.
+    * Implement validation for the size attribute.
+
+"""
+
 class Square:
-    """Represents a square.
-
-    Attributes:
-        size: The size of the square.
-    """
-
     def __init__(self, size):
-        """Initializes a new square with the given size.
+        """Initialize a Square with the given size.
 
         Args:
-            size: The size of the square. Must be an integer greater than or equal to zero.
+            size (int): The size of the square.
+
         """
+        self.__size = size
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be non-negative")
-        else:
-            self.__size = size
-
-    def set_size(self, new_size):
-        """Sets the size of the square.
-
-        Args:
-            new_size: The new size of the square. Must be an integer greater than or equal to zero.
-        """
-
-        if not isinstance(new_size, int):
-            raise TypeError("size must be an integer")
-        elif new_size < 0:
-            raise ValueError("size must be non-negative")
-        else:
-            self.__size = new_size
-
-    def get_size(self):
-        """Returns the size of the square."""
-        return self.__size
-
-    def __str__(self):
-        return f"Square(size={self.__size})"
-
+# Additional documentation for methods, attributes, and functions can be added as needed.
 
 
