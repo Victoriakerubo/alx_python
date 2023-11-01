b5138eed78860aa7da0fe7737d36ed328460c4cb
Victoriakerubo <victoriakerubog@gmail.com>
2023-11-01 11:11:03 +0300

    commit

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,13 +2,14 @@
 
 def inherits_from(obj, a_class):
     """
-    Checks if the object is an instance of a class that inherited from the specified class.
+    Check if the object is an instance of a class that inherited from the specified class.
 
     Args:
         obj: The object to check.
-        a_class: The class to compare against.
+        a_class: The class to compare with.
 
     Returns:
-        bool: True if obj is an instance of a subclass of a_class, False otherwise.
+        True if obj is an instance of a subclass that inherits from a_class, False otherwise.
     """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
+
