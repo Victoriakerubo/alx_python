63ca9812db7ab933906154251edab055c515a55d
Victoriakerubo <victoriakerubog@gmail.com>
2023-11-01 11:39:34 +0300

    commit

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -9,7 +9,7 @@ Attributes:
 
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """A class for representing rectangles.
