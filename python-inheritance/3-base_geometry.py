9da44fdf1c43b4ebea2212fb4b67da5341c81a11
Victoriakerubo <victoriakerubog@gmail.com>
2023-11-01 11:17:28 +0300

    commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,58 @@
 #!/usr/bin/python3
 
+# -*- coding: utf-8 -*-
+"""Geometry module.
+
+This module defines the `BaseGeometry` class, which serves as a base class for geometry-related classes.
+
+Attributes:
+    None
+
+Todo:
+    * Define subclasses for specific geometries like Rectangle, Square, etc.
+    * Implement methods for geometry calculations.
+
+"""
+
 class BaseGeometry:
+    """BaseGeometry class.
+
+    This class provides the foundation for geometry-related classes and includes basic geometric methods.
+
+    Attributes:
+        None
+
     """
-    An empty base class for geometry-related classes.
-    """
-    pass
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        """Calculate the area of the geometric shape.
+
+        Returns:
+            float: The area of the shape.
+
+        """
+        pass
+
+    def perimeter(self):
+        """Calculate the perimeter of the geometric shape.
+
+        Returns:
+            float: The perimeter of the shape.
+
+        """
+        pass
+
+    def __str__(self):
+        """Convert the object to a string representation.
+
+        Returns:
+            str: A string representation of the object.
+
+        """
+        pass
+
+# Define specific geometry subclasses and implement their methods here.
+
